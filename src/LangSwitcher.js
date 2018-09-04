76a5e084ad2a0197ab9bf9e5d38160bdd295045b
fruiz7 <frances.ruiz@gmail.com>
2018-09-04 14:39:45 -0400

    Move languages for LangSwitcher out to a settings file

--- src/LangSwitcher.js
+++ src/LangSwitcher.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withLanguage } from './LanguageContext';
+import langSwitcherSettings from './langSwitcherSettings';
 import './LangSwitcher.css';
 
 const LangSwitcher = props => (
@@ -16,27 +17,22 @@ const LangSwitcher = props => (
       {props.language.label}
       <i className="arrow-down"></i>
     </button>
-    <div id="langSwitcherDropdown" className={`dropdown-content ${props.language.dropdownOpen ? 'show' : ''}`}>
-      <a
-        href="#"
-        onClick={() => props.language.switchLanguage(
-          'English',
-          'English',
-          false)
-        }
-      >
-        English
-      </a>
-      <a
-        href="#"
-        onClick={() => props.language.switchLanguage(
-          'Spanish',
-          'Espa\xF1ol',
-          false)
-        }
-      >
-        {'Espa\xF1ol'}
-      </a>
+    <div
+      id="langSwitcherDropdown"
+      className={`dropdown-content ${props.language.dropdownOpen ? 'show' : ''}`}
+    >
+      {langSwitcherSettings.languages.map(lang => (
+        <a
+          href="#"
+          onClick={() => props.language.switchLanguage(
+            lang.language,
+            lang.label,
+            false)
+          }
+        >
+          {lang.label}
+        </a>
+      ))}
     </div>
   </div>
 );
---
+++ src/langSwitcherSettings.js
@@ -0,0 +1,14 @@
+const langSwitcherSettings = { // eslint-disable-line
+  languages: [
+    {
+      language: 'English',
+      label: 'English',
+    },
+    {
+      language: 'Spanish',
+      label: 'Espa\xF1ol',
+    },
+  ],
+};
+
+export default langSwitcherSettings;
