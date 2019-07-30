ffde4f20eea9d79f8a40c6e7ade5a49702bf9b39
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-07-30 10:43:29 -0400

    Fix error boundary

--- src/template/shared/ErrorBoundary.js
+++ src/template/shared/ErrorBoundary.js
@@ -8,20 +8,20 @@ import config from 'app/config';
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { error: null, errorInfo: null };
   }
 
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, info) {
-    console.log(error, info);
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error,
+      errorInfo,
+    })
+    // You can also log error messages to an error reporting service here
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.errorInfo) {
       // You can render any custom fallback UI
       return (
         <div className="alert alert-danger alert-sm">
